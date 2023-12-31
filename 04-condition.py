14adcab53d1b7f1f89c07511466f76565caeaeaf
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2023-12-31 14:05:13 +0530

    Python Code

--- 04-condition.py
+++ 04-condition.py
@@ -1,6 +1,27 @@
 x = 10
+
+# Simple IF
+# if expression:
+#   code snippet
+
+# If - Else
 if x>10:
     print('x is greater than 10')
 else:
     print('x is not greater than 10')
+
+# Else if
+# <30 Fail
+# 30-70 Pass
+# > 70 Distinction
+
+y = 50
+if y > 70:
+    print('Distinction')
+elif y <= 30:
+    print('Fail')
+else:
+    print('Pass')
+
+
 
