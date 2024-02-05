6cdf0827736bb58abed7d9c48925250c1d949eff
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-05 19:33:03 +0530

    Python Loops

---
+++ 05-loops.py
@@ -0,0 +1,10 @@
+# Two Loops
+#   1. For Loop
+#   2. While Loop
+
+# Syntax - for loop
+# for var in values:
+#   statements
+
+for fruit in ["apple", "banana", "orange"]:
+    print("Fruit Name - %s", fruit)
